f2258f5079dfae34fb371baa4d2340e2c6d64c01
wuyax <pengqingkai@gmail.com>
2019-02-18 16:17:26 +0800

    wechat browser

--- script/index.js
+++ script/index.js
@@ -1,8 +1,12 @@
 window.onload = function() {
+  let openInBrowser = document.querySelector('.open-in-browser')
   let info = new Browser();
   if(info.device === 'Mobile'){
     if(info.browser === 'Wechat'){
-      window.location.href = 'sinaweibo://detail/?dispMeIfNeed=1&mblogid=GqOZwpVr5'
+      openInBrowser.html = '在safari中打开'
+    }
+    if(info.browser === 'Safari'){
+      window.location.href = 'sinaweibo://detail/?dispMeIfNeed=1&mblogid=GqOZwpVr5'      
     }
     
   } else {
