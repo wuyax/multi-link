c49d288842aa4273d4f737f886b37bd8ce5998c1
wuyax <pengqingkai@gmail.com>
2019-02-18 16:06:43 +0800

    add test

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
-(function() {
+window.onload = function() {
+  let info = new Browser();
+  if(info.device === 'Mobile'){
+    if(info.browser === 'Wechat'){
+      window.location.href = 'sinaweibo://detail/?dispMeIfNeed=1&mblogid=GqOZwpVr5'
+    }
+    
+  } else {
 
-}())
+  }
+}
